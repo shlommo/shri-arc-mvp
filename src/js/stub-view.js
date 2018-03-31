3405e14c7b8f27d36c15d99146721184f8d6eeb8
Sayan Zhakupbekov <szhaqypbek@gmail.com>
2018-03-31 19:52:56 +0300

    представление для приложения

---
+++ src/js/stub-view.js
@@ -0,0 +1,48 @@
+/* eslint-disable no-underscore-dangle, class-methods-use-this */
+import View from './mvp/view';
+import StubPresenter from './stub-presenter';
+
+class StubView extends View {
+  constructor(inputData) {
+    super(inputData);
+    this._containerClassName = 'view-stub';
+    this.label = this.inputData.label;
+    this.updateLabelHandler = this.updateLabelHandler.bind(this);
+  }
+
+  getMarkup() {
+    const viewStubInputBlock = `<div class="view-stub__input-block">
+                                  <input class="view-stub__input"/>
+                                  <button class="view-stub__apply">Отправить на сервер</button>
+                                </div>`;
+    const viewStubLabel = `<p class="view-stub__label">${this.label}</p>`;
+
+    return `<div class="${this._containerClassName}">
+              ${viewStubInputBlock}
+              ${viewStubLabel}
+            </div>`;
+  }
+
+  updateLabelHandler() {
+    const viewStubInput = this._element.querySelector('.view-stub__input');
+    const inputValue = viewStubInput.value;
+    if (inputValue.length === 0) {
+      return;
+    }
+    const label = `Сервер принял данные: ${inputValue}`;
+
+    this.clearHandlers();
+    StubPresenter.updateLabel(label);
+  }
+
+  bindHandlers() {
+    this.viewStubBtn = this._element.querySelector('.view-stub__apply');
+    this.viewStubBtn.addEventListener('click', this.updateLabelHandler);
+  }
+
+  clearHandlers() {
+    this.viewStubBtn.removeEventListener('click', this.updateLabelHandler);
+  }
+}
+
+export default (data, containerSelector) => new StubView(data, containerSelector);
