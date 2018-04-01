1f3e316e2d3081daac5ac5c08e93c52d01084ece
Sayan Zhakupbekov <szhaqypbek@gmail.com>
2018-04-01 12:11:28 +0300

    более умные базовый класс презентера

--- src/js/mvp/presenter.js
+++ src/js/mvp/presenter.js
@@ -1,7 +1,39 @@
 /* eslint-disable no-underscore-dangle, class-methods-use-this */
+import Logger from '../utils/logger';
+
 class Presenter {
   constructor(model) {
     this._model = model;
+    this._view = null;
+    this._qState = null;
+    this._updatedView = null;
+  }
+
+  init(view) {
+    Logger.log('Прошла инициализация');
+    this._bindView(view);
+    this._renderView();
+  }
+
+  _bindView(view) {
+    this._view = view;
+    Logger.log('Привязал представление к презентеру');
+  }
+
+  _getQState() {
+    this._qState = this._model.getState();
+    Logger.log('Получил текущее состояние модели');
+  }
+
+  _updateViewData() {
+    this._getQState();
+    this._updatedView = this._view(this._qState);
+  }
+
+  _renderView() {
+    this._updateViewData();
+    this._updatedView.renderView();
+    Logger.log('Отрендерил представление');
   }
 }
 
