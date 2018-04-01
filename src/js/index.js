d40534e01ca029c766e950c5cea5a55f23c30e5c
Sayan Zhakupbekov <szhaqypbek@gmail.com>
2018-04-01 12:28:51 +0300

    удаление лишних коментов

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,4 @@
-// import logger from './utils/logger';
 import stubView from './views/stub-view';
-// import stubData from './data/app-data';
 import StubPresenter from './stub-presenter';
 
 StubPresenter.init(stubView);
-
-// stubView(stubData).renderView();
