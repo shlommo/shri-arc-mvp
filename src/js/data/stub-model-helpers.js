7c9a228f9c344cdf92d4fd993b816e18722f2551
Sayan Zhakupbekov <szhaqypbek@gmail.com>
2018-04-01 12:38:55 +0300

    документирует stub-model-helpers

--- src/js/data/stub-model-helpers.js
+++ src/js/data/stub-model-helpers.js
@@ -1,4 +1,10 @@
 /* eslint-disable import/prefer-default-export */
+/**
+ * Обновляет label в моделе данных
+ * @param  {Object} dataObj - текущее состояние данных
+ * @param  {string} label - значение на которое нужно изменит данные
+ * @returns {Object} - новое состояние данных
+ */
 const setLabel = (dataObj, label) => {
   if (typeof label !== 'string') {
     throw new RangeError('Label must be a string');
